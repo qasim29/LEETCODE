f517e37766c81c11f235f0833ad16a439531d27c
qasim.29 <qasimmohammad534@gmail.com>
2022-10-02 02:50:01 +0500

    problem

---
+++ 238. Product of Array Except Self.py
@@ -0,0 +1,20 @@
+# def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+def productExceptSelf(nums):
+    parr = []
+    sarr=[]
+    result=[]
+    prev=1
+    for i in nums:
+        parr.append(prev*i)
+        prev=prev*i
+ 
+    prev=1
+    for i in range(len(nums)-1,-1,-1):
+        sarr.insert(0,prev*nums[i])
+        prev=prev*nums[i]git
+    print(nums)
+    print(parr)
+    print(sarr)
+
+productExceptSelf([1,2,3,4])
