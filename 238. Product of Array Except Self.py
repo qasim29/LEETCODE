ba7ee1c9eb56eef8d21b488e4cc26cc50b289ba6
qasim.29 <qasimmohammad534@gmail.com>
2022-10-11 21:55:03 +0500

    productExceptSelf

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -12,9 +12,18 @@ def productExceptSelf(nums):
     prev=1
     for i in range(len(nums)-1,-1,-1):
         sarr.insert(0,prev*nums[i])
-        prev=prev*nums[i]
+        prev=prev*nums[i]    
+    
+    result.append(sarr[1])
+    ptr=0
+    while(ptr<len(nums)-2):
+        result.append(parr[ptr]*sarr[ptr+2])
+        ptr+=1
+    result.append(parr[ptr])
+        
     print(nums)
     print(parr)
     print(sarr)
+    print(result)
 
 productExceptSelf([1,2,3,4])
